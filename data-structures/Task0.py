2e441492586fd11a54e0fe718b9f990ac04e1009
pree254 <pripetym.pm@gmail.com>
2025-12-27 00:36:21 +0300

    done task 0

--- data-structures/Task0.py
+++ data-structures/Task0.py
@@ -1,1 +1,12 @@
-print("Hello Chalo")
+#!/usr/bin/python3
+
+# Python function that prints all integers of a list, one per line
+# Without importing modules or casting integers into strings
+
+
+# The user is expected to pass in only a list of integers
+
+def print_all_int(list):
+    for int in list:
+        print(int)
+print_all_int([1, 2, 3, 4, 5])        

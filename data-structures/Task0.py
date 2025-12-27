6cd6d7c6b5c24d8d23339ee83a39ab5b9a1b8153
pree254 <pripetym.pm@gmail.com>
2025-12-27 14:51:46 +0300

    Task1

--- data-structures/Task0.py
+++ data-structures/Task0.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 
+#Task0
+
 # Python function that prints all integers of a list, one per line
 # Without importing modules or casting integers into strings
 
@@ -9,4 +11,21 @@
 def print_all_int(list):
     for int in list:
         print(int)
-print_all_int([1, 2, 3, 4, 5])        
+print_all_int([1, 2, 3, 4, 5])
+
+
+#Task1
+
+#Python function that retrieves an element from a list.
+#If idx is negative or out of range (greater than the number of elements in my_list), the function
+#returns None.
+#Without importing modules or using try/except.
+
+def el_at_list(list,idx):
+    my_list_len = len(list)
+    if idx >= my_list_len or idx < 0:
+        return None
+    else:
+        return list[idx]
+
+print(el_at_list([-1, -2, -3],3))
